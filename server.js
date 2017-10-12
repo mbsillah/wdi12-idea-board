ebda7cff2fe0bb267ae5fa263551fbbf8e439e69
sillah2010 <macktastic2014@gmail.com>
2017-10-12 10:59:22 -0400

    Created basic express layout

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+const bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+
+
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log("Succesfully Connected to MongoDB")
+})
+
+connection.on('error', (error) => {
+    console.log("Mongo Error ", error )
+})
+
+app.use(bodyParser.json())
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('App is listening on port: ', PORT)
+})
